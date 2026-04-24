177afefb56a702d210d8cb1f5bcc6a38e05a65ce
ftmartins <felipetm@sas.upenn.edu>
2026-04-24 13:35:56 -0400

    merge before allosteric ensemble fix

--- runners/submit_allosteric.sh
+++ runners/submit_allosteric.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -t 5-00:00:00
-#SBATCH --qos=low
-#SBATCH --partition=low
+#SBATCH --qos=liu
+#SBATCH --partition=liu_tesla,liu_titan
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
