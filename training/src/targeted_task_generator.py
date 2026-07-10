605c8becad6c36d8f211507a967dd712f3201982
ftmartins <108759611+ftmartins@users.noreply.github.com>
2026-07-10 11:59:20 -0400

    local before changes to tasks 16-23

--- training/src/targeted_task_generator.py
+++ training/src/targeted_task_generator.py
@@ -31,7 +31,7 @@ ENSEMBLE_DIR = BASE_DIR
 TARGETED_RESULTS_DIR = Path('/data2/shared/felipetm/auxetic_networks/targeted_results_sqr')
 
 N_STEPS = 5_000
-N_STRAIN_STEPS = 400    # Same for all targeted tasks
+N_STRAIN_STEPS = 200    # Same for all targeted tasks
 N_TARGETED_NODES = 100  # Same network size for all targeted tasks
 PACKING_SEED = 42  # Same network topology for all tasks
 
